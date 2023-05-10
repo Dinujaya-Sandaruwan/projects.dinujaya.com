08e35c3d44e35f543e3b697fe9999fda414feefd
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-10 14:32:41 +0530

    Build animated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Preview from './components/Preview';
+import { Main } from './components/Main';
 
 function App() {
     return (
         <div className="App">
-            <Preview />
+            <Main />
         </div>
     );
 }
