12d2d4d2b49a073d0b357664237ba6c4bb23bbc8
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-10 17:14:10 +0530

    Added title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Main } from './components/Main';
 import { Card } from './components/Card';
-
+import Title from './components/Title';
 function App() {
     return (
         <div className="App">
             <Main />
+            <Title />
             <div className="data">
                 <Card />
                 <Card />
---
+++ src/components/Title.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// import './styles.css';
+
+const Title: React.FC = () => {
+    return (
+        <div className="waviy">
+            {/* <span style={{ '--i': 1 } as React.CSSProperties}>D</span>
+            <span style={{ '--i': 2 } as React.CSSProperties}>I</span>
+            <span style={{ '--i': 3 } as React.CSSProperties}>N</span>
+            <span style={{ '--i': 4 } as React.CSSProperties}>U</span>
+            <span style={{ '--i': 5 } as React.CSSProperties}>J</span>
+            <span style={{ '--i': 6 } as React.CSSProperties}>A</span>
+            <span style={{ '--i': 7 } as React.CSSProperties}>Y</span>
+            <span style={{ '--i': 8 } as React.CSSProperties}>A</span>
+            <span style={{ '--i': 9 } as React.CSSProperties}>'</span>
+            <span style={{ '--i': 10 } as React.CSSProperties}>&nbsp;</span>
+            <span style={{ '--i': 11 } as React.CSSProperties}>P</span>
+            <span style={{ '--i': 12 } as React.CSSProperties}>R</span>
+            <span style={{ '--i': 13 } as React.CSSProperties}>O</span>
+            <span style={{ '--i': 14 } as React.CSSProperties}>J</span>
+            <span style={{ '--i': 15 } as React.CSSProperties}>E</span>
+            <span style={{ '--i': 16 } as React.CSSProperties}>C</span>
+            <span style={{ '--i': 17 } as React.CSSProperties}>T</span>
+            <span style={{ '--i': 18 } as React.CSSProperties}>S</span> */}
+
+            <span style={{ '--i': 1 } as React.CSSProperties}>M</span>
+            <span style={{ '--i': 2 } as React.CSSProperties}>Y</span>
+            <span style={{ '--i': 3 } as React.CSSProperties}>&nbsp;</span>
+            <span style={{ '--i': 4 } as React.CSSProperties}>P</span>
+            <span style={{ '--i': 5 } as React.CSSProperties}>R</span>
+            <span style={{ '--i': 6 } as React.CSSProperties}>O</span>
+            <span style={{ '--i': 7 } as React.CSSProperties}>J</span>
+            <span style={{ '--i': 8 } as React.CSSProperties}>E</span>
+            <span style={{ '--i': 9 } as React.CSSProperties}>C</span>
+            <span style={{ '--i': 10 } as React.CSSProperties}>T</span>
+            <span style={{ '--i': 11 } as React.CSSProperties}>S</span>
+        </div>
+    );
+};
+
+export default Title;
